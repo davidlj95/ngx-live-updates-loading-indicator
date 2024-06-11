544201b9108af54b4047496af4865f7581fc4d20
David LJ <mail@davidlj95.com>
2024-06-11 19:45:51 +0200

    feat: add PoC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { DataComponent } from "./data.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataComponent],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-
-    <router-outlet />
+    <h1>Live updates loading indicator PoC</h1>
+    <span>As <a
+      href="https://www.reddit.com/r/Angular2/comments/1ddd1o0/can_an_observable_send_null_while_it_is_executing/"> response to a Reddit post in <code>r/Angular2</code></a></span>
+    <h2>👇 Live update component</h2>
+    <app-data></app-data>
+    <router-outlet/>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'live-updates-loading-indicator';
 }
---
+++ src/app/data.component.ts
@@ -0,0 +1,64 @@
+import { ApplicationRef, Component, Inject, Injectable, PLATFORM_ID } from "@angular/core";
+import { delay, exhaustMap, first, interval, merge, Observable, of, tap } from "rxjs";
+import { AsyncPipe, isPlatformBrowser } from "@angular/common";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: "app-data",
+  standalone: true,
+  imports: [AsyncPipe],
+  template: `
+    @if (data$ | async; as data) {
+      <pre>{{ data }}</pre>
+    } @else {
+      <span>⌛️ Loading...</span>
+    }
+  `,
+  styles: ``,
+})
+export class DataComponent {
+  protected data$?: Observable<string | null>;
+
+  constructor(
+    private readonly appRef: ApplicationRef,
+    @Inject(PLATFORM_ID) private readonly platformId: object,
+    private readonly dataService: DataService,
+  ) {
+    // 👇 Init clock after app is stable. Or it will never be stable.
+    this.appRef.isStable.pipe(
+      first((isStable) => isStable),
+      tap(() => {
+        // 👇 On server we want to render and respond. Live updates only on browser
+        if (isPlatformBrowser(this.platformId)) {
+          this.data$ = this.dataService.clock().pipe(
+            // This can be also:
+            // https://rxjs.dev/api/index/function/concatMap
+            // https://rxjs.dev/api/index/function/switchMap
+            // https://rxjs.dev/api/index/function/mergeMap
+            // Depending on how do you want the outer (clock) and inner (data)
+            // observables. Check the marble diagrams of docs for more info
+            exhaustMap(
+              (i) => merge(
+                of(null), // 👈 Indicate right now we're about to fetch
+                this.dataService.getData(i) // and fetch
+              ),
+            ),
+          )
+        }
+      }), takeUntilDestroyed()).subscribe()
+  }
+}
+
+@Injectable({ providedIn: "root" })
+class DataService {
+  clock(): Observable<number> {
+    return merge(
+      of(-1), // Simulate an initial value,
+      interval(2000) // Simulate live updates every 2s,
+    );
+  }
+
+  getData(i: number): Observable<string> {
+    return of(`Fetched data #${i}`).pipe(delay(500));
+  }
+}
